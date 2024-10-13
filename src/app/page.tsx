f2a64a3315220fc962cc493b3cdb76a741c141a3
Muhammad Muneeb Siddique <siddiquimuneeb894@gmail.com>
2024-10-13 12:31:30 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 
 import Main from "../app/components/main/page"
 import Footer from "../app/components/footer/page"
-import  Header from "../app/components/header/page"
 
 export default function Home() {
   return (
     <div>
-        <Header/>
         <Main/>
         <Footer/>
     </div>
