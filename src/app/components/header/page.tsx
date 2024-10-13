d80ec84008c5720c862e444fbb7d680359cdef16
Muhammad Muneeb Siddique <siddiquimuneeb894@gmail.com>
2024-10-13 12:14:04 -0700

    Update page.tsx

--- src/app/components/header/page.tsx
+++ src/app/components/header/page.tsx
@@ -8,9 +8,9 @@ export default function Header(){
             <div className={css.nav_bar}>
                 <ul>
                     <li><a href="/">Home</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Skill</a></li>
-                    <li><a href="">Contact</a></li>
+                    <li><a href="./#about">About</a></li>
+                    <li><a href="./#skill">Skill</a></li>
+                    <li><a href="./#contact">Contact</a></li>
                 </ul>
             </div>
         </header>
